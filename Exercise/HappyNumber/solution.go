7ecc2cebcadd0788b3047648927ebd723f414a81
joewalker <assassin.joe@hotmail.com>
2020-04-22 15:38:29 +0700

    add exercise happy number

---
+++ Exercise/HappyNumber/solution.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := isHappyNumber(17)
+	fmt.Println(x)
+}
+
+func isHappyNumber(n int) bool {
+	slow, fast := n, n
+	for {
+		slow = numSquareSum(slow)
+		fast = numSquareSum(numSquareSum(fast))
+		if slow == fast {
+			break
+		}
+	}
+	return slow == 1
+}
+
+func numSquareSum(n int) int {
+	sum := 0
+	for n != 0 {
+		sum += (n % 10) * (n % 10)
+		n /= 10
+	}
+
+	return sum
+}
